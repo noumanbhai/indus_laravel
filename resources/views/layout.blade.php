b1ea77a8bcaec96f1c10197d173d79bf1c2f2595
noumanbhai <noumanbhai006@gmail.com>
2018-12-06 17:21:33 +0500

    master file
    @yield('content')

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+
+    @yield('content')    
+    
+    <ul>
+        <li>
+            <a href="/contact">Contact</a>       
+        </li>
+        <li>
+            <a href="/aboute">Aboute</a>       
+        </li>
+    </ul>
+
+    
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Home</title>
-</head>
-<body>
+@extends('layout')
 
-    <h1>Home Page!</h1>
-    
-    <ul>
-        <li>
-            <a href="/contact">Contact</a>       
-        </li>
-        <li>
-            <a href="/aboute">Aboute</a>       
-        </li>
-    </ul>
-    
-</body>
-</html>
+
+
+
+
+@section('content')
+<h1>Home page:</h1>
+@endsection
+
+
