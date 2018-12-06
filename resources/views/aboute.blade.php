09db1a0bf1fe9a1ed3b85ed40f39cde79bebdf9a
noumanbhai <noumanbhai006@gmail.com>
2018-12-06 18:09:50 +0500

    title dynamic
    masterfile
        <title>@yield('title')</title>
    aboute page
    @section('title','Aboute Us')
    contact page
    @section('title','Contact Us')

--- resources/views/aboute.blade.php
+++ resources/views/aboute.blade.php
@@ -1,7 +1,8 @@
 @extends('layout')
 @section('content')
 
-<h1>Aboute page</h1>
+@section('title','Aboute Us')
+<h1>Aboute US:</h1>
 
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas pariatur, possimus esse recusandae ex quisquam obcaecati mollitia culpa magni, dolorem quos voluptatem similique? Temporibus laborum corporis blanditiis praesentium, dolorum, aut.</p>
 
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,7 +3,10 @@
 	
 @section('content')
 
-	<h1>This is Contact page:</h1>
+@section('title','Contact Us')
+
+<h1>Contact US:</h1>
+
 
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam ea iure tenetur, dolore, enim architecto magnam quidem accusantium placeat eos non ex voluptatum reprehenderit dolores est vel veroquatur Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi, exercitationem nulla magni voluptatibus rem aut quod cum ipsa culpa id corporis libero ullam deserunt maiores voluptatem. Doloribus, eos, illo. </p> 
 
